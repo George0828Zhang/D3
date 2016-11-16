68c88715bd2b10636e576470db872008e196925d
George0828Zhang <george0828zhang@gmail.com>
2016-11-16 18:28:14 +0800

    11/16 add js

---
+++ readcsv.js
@@ -0,0 +1,25 @@
+	function nvamd()
+	{
+	var width = 1920; var height = 1080;
+	var svgctrl = d3.select(".article").append("svg").attr("width", width).attr("height", height);
+	var len;
+	var max_y;
+
+	d3.csv("https://george0828zhang.github.io/D3/NvidiaStock.csv", function(data)
+		{
+			len = data.length;
+			console.log(data);
+			max_y = d3.max(data, function(d){return d.close});
+			var lines = d3.line().x(function(d, i){return i * (width/len);}).y(function(d){return height*(1 - d.close / max_y)});
+			svgctrl.append("path").data([data]).attr("d", lines).attr("stroke", "green").attr("stroke-width", 3).attr("fill", "none");
+		}
+	);
+
+	d3.csv("https://george0828zhang.github.io/D3/AMDStock.csv", function(data)
+		{
+			console.log(data);
+			var lines = d3.line().x(function(d, i){return i * (width/len);}).y(function(d){return height*(1 - d.close / max_y)});
+			svgctrl.append("path").data([data]).attr("d", lines).attr("stroke", "red").attr("stroke-width", 3).attr("fill", "none");
+		}
+	);
+	}
